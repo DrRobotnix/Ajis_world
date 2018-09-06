f78a562c877ead812b8c199b73215242e2046ad6
Elizabeth McCarthy <elizabeth@Elizabeths-MacBook-Pro-2.local>
2018-09-06 10:46:27 -0700

    Adding Hammand distance function and for loop to calc distance

--- fastq_to_ham_plot.py
+++ fastq_to_ham_plot.py
@@ -3,10 +3,9 @@
 
 # Script must ultimately be contained in one fluid .py file merged via GitHub
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 f = open(CTGATC.fastq)
-
 def getSeqs(fastq_file):
 	#Parse a FASTQ for sequence identities and corresponding sequences
 	seqences = {}
@@ -14,8 +13,22 @@ def getSeqs(fastq_file):
 
 def hamDist(str1, str2):
    #Count the # of differences between equal length strings str1 and str2
-   diffs = 0
-   return diffs
+	diffs = 0
+	for i in range(len(str1)):
+		if str1[i] != str2[i]:
+			diffs = diffs +1
+	return diffs
+
+orig_data = getSeqs(f)
+#orig_data = {"#1":"AAGGTTNTC","#2":"ATGTTTNTC", "#3":"ATGTTTGTC"}
+seq_data = list(orig_data.values())
+output = list()
+for j in range(len(seq_data)):
+		for i in range((j+1),len(seq_data)):
+			if seq_data[i] != seq_data[j]:
+				output.append(hamDist(seq_data[j],seq_data[i]))
+
+#print(output)
 
 #Make some kind of plot that contains the data you've calculated.
-plt.show()
+#plt.show()
